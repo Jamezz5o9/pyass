acc88d7ff906c636487827436013fd119513e36b
Aduloju James <adulojujames@gmail.com>
2022-08-25 09:24:38 +0100

    commit header tag

--- grade.py
+++ grade.py
@@ -2,10 +2,10 @@ student_record = []
 
 
 def gathering_student_info():
-    keep_adding_info = "yes" or 'Y' or "Yes" or "YES" or "Y"
-    new_add = keep_adding_info
+
+    keep_adding_info = "y"
     count = 0
-    while keep_adding_info == new_add:
+    while keep_adding_info == "y":
         name = input("Please Enter your name: ")
         first_grade = int(input("Enter math score: "))
         second_grade = int(input("Enter your eng score: "))
@@ -28,6 +28,8 @@ def gathering_student_info():
 
 
 def output_grade():
+    print("#################################################################")
+    print("{}{:<10s}{:<10s}{:<10s}{:<10s}{:<10s}".format("Names", "Math", "Eng", "Total", "Average", "Position"))
     for i in range(len(student_record)):
         print(student_record[i]["name"], "\t", student_record[i]["first_grade"], "\t",
               student_record[i]["second_grade"], "\t",
